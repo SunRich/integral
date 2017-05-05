578e094414e5c83c626762ca10f4d9acb63d6d33
SunRich <13257001077@126.com>
2017-05-05 13:52:39 +0800

    add dockerfile

---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo 'write environment variables..'
+export APP_DEBUG=false
+export APP_ENV=production
+export APP_KEY=SomeRandomKey!!!
+export DB_CONNECTION=mysql
+export DB_HOST=$MYSQL_PORT_3306_TCP_ADDR
+export DB_PORT=$MYSQL_PORT_3306_TCP_PORT
+export DB_DATABASE=signin
+export DB_USERNAME=root
+export DB_PASSWORD=$MYSQL_ENV_MYSQL_ROOT_PASSWORD
+export CACHE_DRIVER=array
+export QUEUE_DRIVER=array
+export APP_TIMEZONE=PRC
+export DB_TIMEZONE=+08:00
+echo 'change to workdir'
+cd /var/www/webapp;
+echo "start migrate"
+php artisan migrate --force
+echo "migrate finshed"
+
+echo 'start apache..'
+a2enmod rewrite
+apache2-foreground
